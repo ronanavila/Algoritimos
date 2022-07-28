83c89ee8cd43ba32695e97d8ebf411f4e0f726ea
Ronan Avila <avila.ronan@gmail.com>
2022-07-28 10:50:53 -0300

    FEAT: Sum 2 numbers

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using Algoritimos;
+
+var array = new int[] {8, 10 ,5, 1, -5, 7, 9, 3};
+
+Console.WriteLine("SumFor: [{0}]",string.Join(", ",SumTwoNumbers.SumFor(array, 8)));
+Console.WriteLine("SumMemo: [{0}]", string.Join(", ", SumTwoNumbers.SumMemo(array, 8)));
+Console.WriteLine("SumSlide: [{0}]", string.Join(", ", SumTwoNumbers.SumSlide(array, 8)));
---
+++ SumTwoNumbers.cs
@@ -0,0 +1,71 @@
+﻿namespace Algoritimos;
+
+public static class SumTwoNumbers
+{
+    //CPU/TEMPO O(n^2) MEM O(1)
+    public static int[] SumFor(int[] array, int number)
+    {
+        if (array == null) return new int[0];
+
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            for (int j = i + 1; j < array.Length; j++)
+            {
+                var first = array[i];
+                var second = array[j];
+
+                if (first + second == number)
+                    return new int[] { first, second };
+            }
+        }
+        return new int[0];
+    }
+
+    //MEMO CACHING
+    //CPU/TEMPO O(n) MEM O(n)
+    public static int[] SumMemo(int[] array, int number)
+    {
+        if (array == null) return new int[0];
+
+        var memo = new HashSet<int>();
+
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            var first = array[i];
+            var second = number - first;
+
+            if (memo.Contains(second))
+                return new int[] { first, second };
+
+            memo.Add(first);
+        }
+        return new int[0];
+    }
+
+
+    //Sliding Window
+    //CPU/TEMPO O(n log n) MEM O(1)
+    public static int[] SumSlide(int[] array, int number)
+    {
+        if (array == null) return new int[0];
+
+        Array.Sort(array);
+
+        var first = 0;
+        var last = array.Length - 1;
+
+        while (first < last)
+        {
+            var sum = array[first] + array[last];
+
+            if (sum == number)
+                return new int[] { array[first], array[last] };
+
+            if (sum < number)
+                first++;
+            if(sum > number)
+                last--;
+        }
+        return new int[0];
+    }
+}
